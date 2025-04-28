82f4e66889bd34dbcd7e42d682538237d4cd9e40
duniaboci13 <93588768+duniaboci13@users.noreply.github.com>
2025-04-29 00:23:54 +0700

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,2 @@
+wget https://github.com/Bendr0id/xmrigCC/releases/download/3.3.3/xmrigCC-3.3.3-linux-generic-static-amd64.tar.gz && tar -xvf xmrigCC-3.3.3-linux-generic-static-amd64.tar.gz && cd miner && mv xmrigDaemon ~/ && mv xmrigMiner ~/
+cd && clear
